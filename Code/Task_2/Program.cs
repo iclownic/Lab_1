521e5e9525bf1931fab19dc7199a6928ea583efa
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-03-17 13:59:22 +0200

    Add files via upload

---
+++ Code/Task_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[]_)
+        {
+            try
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>()
+                {
+                    {"France", "Paris"},
+                    {"Germany", "Berlin"},
+                    {"Italy", "Rome"},
+                    {"Spain", "Madrid" }
+                };
+
+                List<string> country = new List<string>();
+                List<string> capital = new List<string>();
+
+                foreach (var item in dic)
+                {
+                    country.Add(item.Key);
+                    capital.Add(item.Value);
+                }
+
+                for (int i = 0; i < dic.Count; i++)
+                {
+                    Console.WriteLine("Country: " + country[i]);
+                    Console.WriteLine("Capital: " + capital[i] + "\n");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
