abc0d4f29f7e022b79b36adf603168dd8725898b
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-03-17 13:42:26 +0200

    Add files via upload

---
+++ Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[]_)
+        {            
+            try
+            {
+                List<int> list = new List<int>() { -21, 33, 146, 53, 4 };
+                var selectedList = list.Where(i => i > 0).Select(i => i % 10).Distinct();
+
+                foreach (var i in selectedList)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
